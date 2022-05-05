e31366682a80413f7fbf7cc711ce718670474f0d
unknown <you@example.com>
2022-05-05 08:05:12 -0400

    footer update

--- src/components/organims/footer/FooterDiv.js
+++ src/components/organims/footer/FooterDiv.js
@@ -1,39 +1,45 @@
 import { Box, Typography } from "@mui/material";
 import React from "react";
 import "./styles.css";
-import FacebookIcon from '@mui/icons-material/Facebook';
-import TwitterIcon from '@mui/icons-material/Twitter';
-import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from "@mui/icons-material/Facebook";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
 
 const FooterDiv = () => {
   return (
     <div>
+      <hr />
       <Box className="footerBox">
         <div className="colfooter">
-          <Typography className="titleFooter" variant="h4">Pufi</Typography>
+          <Typography className="titleFooter" variant="h4">
+            Pufi
+          </Typography>
         </div>
         <div className="colfooter">
-          <Typography variant="p">PUFI RAIN</Typography>
+          <Typography variant="subtitle2">PUFI RAIN</Typography>
           <br></br>
-          <Typography variant="p">PUFI PUFF</Typography>
+          <Typography variant="subtitle2">PUFI PUFF</Typography>
           <br></br>
-          <Typography variant="p">PUFI CART</Typography>
+          <Typography variant="subtitle2">PUFI CART</Typography>
           <br></br>
-          <Typography variant="p">PUFI SNAP</Typography>
+          <Typography variant="subtitle2">PUFI SNAP</Typography>
         </div>
         <div className="colfooter">
-          <Typography variant="p">CONTACTO</Typography>
+          <Typography variant="subtitle2">CONTACTO</Typography>
           <br></br>
-          <Typography variant="p">AYUDA</Typography>
+          <Typography variant="subtitle2">AYUDA</Typography>
           <br></br>
-          <Typography variant="p">COMO COMPRAR</Typography>
+          <Typography variant="subtitle2">COMO COMPRAR</Typography>
           <br></br>
-          <Typography variant="p">TERMINOS & CONDICIONES</Typography>
+          <Typography variant="subtitle2">TERMINOS & CONDICIONES</Typography>
         </div>
         <div className="colfooter">
-          COMPRA 100% SEGURA
+          <Typography variant="subtitle2">COMPRA 100% SEGURA</Typography>
         </div>
-        <div className="colfooter">SEGUINOS EN <FacebookIcon/> <TwitterIcon/> <InstagramIcon/> </div>
+        <div className="colfooter">
+        <Typography variant="subtitle2"> SEGUINOS EN <FacebookIcon /> <TwitterIcon /> <InstagramIcon /></Typography>
+         {" "}
+        </div>
       </Box>
     </div>
   );
